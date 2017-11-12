8507f5cd7f75a832a9adc9ded71d380afb048981
OliviaKim6 <32452676+OliviaKim6@users.noreply.github.com>
2017-11-12 19:30:25 +0900

    Update mail.php

--- mail.php
+++ mail.php
@@ -2,16 +2,19 @@
 require_once("class.phpmailer.php")
 $name_01=$_POST['name'];
 $mail_02=$_POST['mail'];
-$head_03=$_POST['subject'];
+$subject=$_POST['subject'];
 $msg_04=$_POST['message'];
 
 $to='dbd03028@gmail.com';
-$subject=$head_03;
 $msg="보낸사람:$name_01\n".
 "보낸사람 메일주소:$mail_02\n".
 "메일내용:$msg_04";
 
 $sendmail = mail($to,$subject,$msg);
+if (!$mail->send()) {
+echo "Mailer Error! ";
+} else {
+echo "메일 전송 완료! 감사합니다. 뒤로 가기를 눌러주세요";
+}
+
 ?>
-
-
